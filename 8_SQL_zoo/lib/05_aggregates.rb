00ab79a246946e570cbc422f595d75f3a1576ca9
Spencer <spenjlee@gmail.com>
2019-07-04 17:38:38 -0700

    Add method to return total population of 3 European countries

--- 8_SQL_zoo/lib/05_aggregates.rb
+++ 8_SQL_zoo/lib/05_aggregates.rb
@@ -55,7 +55,13 @@ end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
-  execute(<<-SQL)
+	execute(<<-SQL)
+		SELECT
+			SUM(population)
+		FROM
+			countries
+		WHERE
+			name IN ('France', 'Germany', 'Spain')
   SQL
 end
 
