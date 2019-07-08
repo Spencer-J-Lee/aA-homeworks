6502d0b0c4258e3a76733b65a97a7afd4064f54c
Spencer <spenjlee@gmail.com>
2019-07-08 16:12:04 -0700

    Add skeleton for Playwright class

--- 9_plays_playwrights_ORM/plays.rb
+++ 9_plays_playwrights_ORM/plays.rb
@@ -65,4 +65,30 @@ class Play
   end
 end
 
-# binding.pry
+class PlayWright
+	def self.all
+
+	end
+	
+	def self.find_by_name(name)
+
+	end
+	
+	def initialize
+		
+	end
+
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def get_plays # returns all plays written by playwright)
+		
+	end
+end
+
+binding.pry
