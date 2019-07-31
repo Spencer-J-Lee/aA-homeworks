70cff64853b117e5c47ceea7699de0e76a4ea4db
Spencer <spenjlee@gmail.com>
2019-07-31 16:18:30 -0700

    Refactor with dynamic dispatch

--- 14_meta_corgis/meta_corgi_snacks.rb
+++ 14_meta_corgis/meta_corgi_snacks.rb
@@ -7,23 +7,35 @@ class MetaCorgiSnacks
     @box_id = box_id
   end
 
-	def method_missing(food_type)
-		if valid_food_type?(food_type)
-			info      = get_info(food_type)
-			tastiness = get_tastiness(food_type)
-			food_type = food_type.capitalize
+	# def method_missing(food_type)
+	# 	if valid_food_type?(food_type)
+	# 		info      = get_info(food_type)
+	# 		tastiness = get_tastiness(food_type)
+	# 		food_type = food_type.capitalize
 
-			result = [food_type, info, tastiness].join(': ')
+	# 		result = [food_type, info, tastiness].join(': ')
 
-			tastiness > 30 ? "* #{result}" : result
-		else
-			super
-		end
-  end
+	# 		tastiness > 30 ? "* #{result}" : result
+	# 	else
+	# 		super
+	# 	end
+  # end
 
   def self.define_snack(name)
-    # Your code goes here...
+		define_method(name) do
+			info      = get_info(name)
+			tastiness = get_tastiness(name)
+			name      = name.capitalize
+
+			result = [name, info, tastiness].join(': ')
+
+			tastiness > 30 ? "* #{result}" : result
+		end
 	end
+
+	define_snack(:bone)
+	define_snack(:kibble)
+	define_snack(:treat)
 
 	private
 
@@ -39,3 +51,10 @@ class MetaCorgiSnacks
 		@snack_box.send("get_#{food_type}_tastiness", @box_id)
 	end
 end
+
+# For Testing
+
+box = MetaSnackBox.new
+snacks = MetaCorgiSnacks.new(box, 3)
+
+binding.pry
