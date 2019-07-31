09adc7ccdec3221ad9995270ca6bf7130999cf13
Spencer <spenjlee@gmail.com>
2019-07-31 15:56:06 -0700

    Implement dynamic finder

--- 14_meta_corgis/meta_corgi_snacks.rb
+++ 14_meta_corgis/meta_corgi_snacks.rb
@@ -1,4 +1,5 @@
 require_relative 'meta_snack_box'
+require 'pry'
 
 class MetaCorgiSnacks
   def initialize(snack_box, box_id)
@@ -6,12 +7,22 @@ class MetaCorgiSnacks
     @box_id = box_id
   end
 
-  def method_missing(name, *args)
-    # Your code goes here...
-  end
+	def method_missing(food_type)
+		valid_methods = [:bone, :kibble, :treat]
 
+		if valid_methods.include?(food_type)
+			info      = @snack_box.send("get_#{food_type}_info", @box_id)
+			tastiness = @snack_box.send("get_#{food_type}_tastiness", @box_id)
+
+			result    = "#{food_type.capitalize}: #{info}: #{tastiness}"
+
+			tastiness > 30 ? "* #{result}" : result
+		else
+			super
+		end
+  end
 
   def self.define_snack(name)
     # Your code goes here...
-  end
+	end
 end
